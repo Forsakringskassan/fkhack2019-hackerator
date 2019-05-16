e501c6b451efa6ac10738c2e0e044308e05dc50c
useless1024 <50697203+useless1024@users.noreply.github.com>
2019-05-16 13:53:37 +0200

    Updated

--- stamplare.py
+++ stamplare.py
@@ -1,10 +1,28 @@
 #!/opt/rh/rh-python36/root/usr/bin/python
 
 import os
+import time
+from datetime import datetime
+
 import db_functions as db
 
+def stampla(rfid):
+    a=db.hamta_anvandare(rfid = rfid)
+    #print(a)
 
-a=db.hamta_anvandare('123,345,456,566')
-print(a)
+    if not a:
+        return None, "Fel: Kortet ar inte registrerat."
 
-db.stampla(a['kortnummer'])
+    status, ts = db.stampla(a['kortnummer'])
+    return status, ts
+
+def stamplingar(kortnummer):
+    a=db.hamta_anvandare(kortnummer = kortnummer)
+    if not a:
+        print("Anvandaren finns inte.")
+
+    alla_stamplingar = db.stamplingar(kortnummer)
+
+    return alla_stamplingar
+
+  
