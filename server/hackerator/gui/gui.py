f3df4d5e4ad526b10c13199bcbcee16acf7fd964
Mikael Viklund <mickevi@gmail.com>
2019-05-16 13:11:13 +0200

    gui

--- server/hackerator/gui/gui.py
+++ server/hackerator/gui/gui.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template
 
-gui_blueprint = Blueprint('ui', __name__, template_folder='templates')
+gui_blueprint = Blueprint('ui', __name__, template_folder='templates', static_folder='gui/static')
 @gui_blueprint.route('/gui')
 def view():
-    return render_template("main.html")
+    return render_template("main_mivi.html")
