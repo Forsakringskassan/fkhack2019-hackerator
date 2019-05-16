b2b9c332e676003fbd63ca7cad118f90d3c00a89
misu <misu02@me.com>
2019-05-16 15:38:02 +0200

    Added jquery.

--- server/hackerator/gui/gui.py
+++ server/hackerator/gui/gui.py
@@ -3,4 +3,4 @@ from flask import Blueprint, render_template
 gui_blueprint = Blueprint('ui', __name__, template_folder='templates', static_folder='gui/static')
 @gui_blueprint.route('/gui')
 def view():
-    return render_template("main_mivi.html")
+    return render_template("main.html")
