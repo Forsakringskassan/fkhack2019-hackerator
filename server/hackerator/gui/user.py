bec90d8b85ffc0d376874ffa469478be7ce6642b
Mikael Viklund <mickevi@gmail.com>
2019-05-16 19:39:52 +0200

    user

--- server/hackerator/gui/user.py
+++ server/hackerator/gui/user.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, request
 import db_functions as db
-
+from json import dumps
 
 user_blueprint = Blueprint('user', __name__, template_folder='templates', static_folder='gui/static')
 @user_blueprint.route('/user/<kortnr>')
@@ -12,5 +12,5 @@ def user(kortnr):
     except IndexError:
         status = 0
     userdata.reverse()
-    return render_template("user.html", kortnummer=kortnr, userdata=userdata[:10],
+    return render_template("user.html", kortnummer=kortnr, userdata=dumps(userdata[:10]),
                            status=status, rfid=user['rfid'], request=request)
