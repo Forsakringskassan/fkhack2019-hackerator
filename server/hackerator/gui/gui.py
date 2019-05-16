69b6eaf0adeeb9fc66fb77b36f28fbd6b530a70f
Mikael Viklund <mickevi@gmail.com>
2019-05-16 11:15:49 +0200

    added gui

---
+++ server/hackerator/gui/gui.py
@@ -0,0 +1,6 @@
+from flask import Blueprint, render_template
+
+gui_blueprint = Blueprint('ui', __name__, template_folder='templates')
+@gui_blueprint.route('/gui')
+def view():
+    return "GUI"
--- server/hackerator/wsgi.py
+++ server/hackerator/wsgi.py
@@ -1,6 +1,9 @@
 from flask import Flask, request, render_template
 from flask_bootstrap import Bootstrap
+from server.hackerator.gui.gui import gui_blueprint
+
 application = Flask(__name__)
+application.register_blueprint(gui_blueprint)
 bootstrap = Bootstrap(application)
 
 @application.route("/")
@@ -10,7 +13,6 @@ def hello():
 @application.route("/toggle/<id>")
 def toggle(id):
     return "Toggle" + id
-
 @application.route("/status/<id>")
 def status(id):
     return "status" + id
