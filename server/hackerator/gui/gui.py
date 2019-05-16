b36cf243ffdfa5118694859a8da66a3f7784573d
Mikael Viklund <mickevi@gmail.com>
2019-05-16 12:46:28 +0200

    gui

--- server/hackerator/gui/gui.py
+++ server/hackerator/gui/gui.py
@@ -3,4 +3,4 @@ from flask import Blueprint, render_template
 gui_blueprint = Blueprint('ui', __name__, template_folder='templates')
 @gui_blueprint.route('/gui')
 def view():
-    return "GUI"
+    return render_template("main.html")
--- server/hackerator/wsgi.py
+++ server/hackerator/wsgi.py
@@ -1,10 +1,13 @@
 from flask import Flask, request, render_template
 from flask_bootstrap import Bootstrap
 from server.hackerator.gui.gui import gui_blueprint
+from flask_socketio import SocketIO, emit
+import time
 
 application = Flask(__name__)
 application.register_blueprint(gui_blueprint)
 bootstrap = Bootstrap(application)
+socketio = SocketIO(application)
 
 @application.route("/")
 def hello():
@@ -12,11 +15,14 @@ def hello():
 
 @application.route("/toggle/<id>")
 def toggle(id):
+    now = time.time()
+    emit('toggle', {'id': id, 'timestmp:': now})
     return "Toggle" + id
+
 @application.route("/status/<id>")
 def status(id):
     return "status" + id
 
 
 if __name__ == "__main__":
-    application.run()
+    socketio.run(application)
